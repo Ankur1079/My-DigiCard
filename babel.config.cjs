098c946522132934c560f25f9159b4c01ab64dd6
Ankur1079 <ankuryadav1079@gmail.com>
2026-09-13 12:00:07 +0530

    fix(jest): update babel runtime and generate real lcov coverage stats

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,7 +1,8 @@
+// babel.config.cjs
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }],
-    ['@babel/preset-react', { runtime: 'automatic' }],
+    ['@babel/preset-react', { runtime: 'automatic' }], // <--- Added automatic runtime
     '@babel/preset-typescript',
   ],
 };
